85187334cd374cd5f7d435267293dea39dec69af
eotkd4791 <eotkd4791@gmail.com>
2025-12-21 13:11:29 +0900

    fix: 정적 리소스 요청은 redirect 통과하도록 수정

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,6 +1,19 @@
 export const onRequest: PagesFunction = async ({ request, next }) => {
   const url = new URL(request.url);
 
+  if (
+    url.pathname.startsWith("/_next") ||
+    url.pathname.startsWith("/assets") ||
+    url.pathname.startsWith("/favicon") ||
+    url.pathname.endsWith(".css") ||
+    url.pathname.endsWith(".js") ||
+    url.pathname.endsWith(".png") ||
+    url.pathname.endsWith(".jpg") ||
+    url.pathname.endsWith(".svg")
+  ) {
+    return next();
+  }
+
   if (url.pathname.startsWith("/ko") || url.pathname.startsWith("/en")) {
     return next();
   }
